b4b6defaa94dd4ead867cd7c7f6136f1d3b4aece
Ned Western <Ned.Western@aemo.com.au>
2025-08-16 17:15:25 +1000

    wip

--- .bash_funcs.sh
+++ .bash_funcs.sh
@@ -8,10 +8,15 @@ This file is managed by the dotfiles repository
 set_quick_pass() {
     echo -n "Quick password: "
     read -s QUICK_PASS
+    export QUICK_PASS
     echo
     bind "\"\C-xx\": \"$QUICK_PASS\""
 }
 
+sudo_with_stored_pass() {
+    echo "$QUICK_PASS" | sudo -p '' -S "$@"
+}
 
 alias qpass='set_quick_pass'
+alias ssudo='sudo_with_stored_pass'
 
