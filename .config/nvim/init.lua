bba5e67ae26e26e1f4a942b446c104d852d32c13
Ned Western <Ned.Western@aemo.com.au>
2026-04-10 20:15:04 +1000

    wip

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -29,57 +29,108 @@ vim.keymap.set('i', 'jk', '<Esc>', { desc = 'Exit insert mode' })
 -- ==========================================================================
 require("lazy").setup({
   
+  -- {
+  --   "catppuccin/nvim",
+  --   name = "catppuccin",
+  --   priority = 1000, -- Ensure the colorscheme loads before anything else
+  --   config = function()
+  --     require("catppuccin").setup({
+  --       flavour = "mocha", -- "latte" is the bright light theme. (Options: latte, frappe, macchiato, mocha)
+  --       transparent_background = false,
+  --       integrations = {
+  --         cmp = true,
+  --         native_lsp = {
+  --           enabled = true,
+  --         },
+  --       },
+  --     })
+  --     
+  --     -- Activate the theme
+  --     vim.cmd.colorscheme("catppuccin")
+  --   end,
+  -- },
+
+  -- {
+  --   "scottmckendry/cyberdream.nvim",
+  --   name = "cyberdream",
+  --   priority = 1000,
+  --   config = function()
+  --     require("cyberdream").setup({
+  --       transparent = false, -- Set to true if you want to force it to use your Git Bash terminal's exact background
+  --       italic_comments = true,
+  --       hide_fillchars = true,
+  --     })
+
+  --     -- Activate the theme
+  --     vim.cmd.colorscheme("cyberdream")
+  --   end,
+  -- },
+
+  {
+    "navarasu/onedark.nvim",
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require('onedark').setup {
+        style = 'darker'
+      }
+      require('onedark').load()
+    end
+  },
+
   -- 1. LSP & Autocomplete (Strictly no npm)
+
   {
     "neovim/nvim-lspconfig",
     dependencies = {
       "hrsh7th/nvim-cmp",
       "hrsh7th/cmp-nvim-lsp",
     },
+
     config = function()
-      local lspconfig = require("lspconfig")
-      local configs = require("lspconfig.configs")
-      local cmp = require("cmp")
+        local cmp = require("cmp")
 
-      -- Autocomplete setup using native Neovim snippets (Requires 0.10+)
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            vim.snippet.expand(args.body) 
-          end,
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
-        }),
-        sources = {
-          { name = "nvim_lsp" },
-        },
-      })
-
-      -- Register Astral's 'ty' LSP
-      if not configs.ty then
-        configs.ty = {
-          default_config = {
-            cmd = { "ty", "server" },
-            filetypes = { "python" },
-            root_dir = lspconfig.util.root_pattern("uv.lock", "pyproject.toml", ".git"),
-            single_file_support = true,
+        -- 1. Autocomplete setup
+        cmp.setup({
+          snippet = {
+            expand = function(args)
+              vim.snippet.expand(args.body) 
+            end,
           },
-        }
-      end
+          mapping = cmp.mapping.preset.insert({
+            ["<C-Space>"] = cmp.mapping.complete(),
+            ["<CR>"] = cmp.mapping.confirm({ select = true }),
+            -- (If you added the custom Tab mappings from earlier, keep them here)
+          }),
+          sources = {
+            { name = "nvim_lsp" },
+          },
+        })
 
-      -- Attach 'ty' LSP and broadcast autocomplete capabilities
-      lspconfig.ty.setup({
-        capabilities = require("cmp_nvim_lsp").default_capabilities(),
-      })
+        -- 2. Native Neovim 0.11+ LSP Configuration 
+        
+        -- Broadcast cmp autocomplete capabilities globally to ALL language servers
+        vim.lsp.config("*", {
+          capabilities = require("cmp_nvim_lsp").default_capabilities(),
+        })
 
-      -- LSP Keymaps: Keyboard-only docstring and signature viewing
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, { desc = 'Show Docstring (Normal)' })
-      vim.keymap.set('i', '<C-k>', vim.lsp.buf.hover, { desc = 'Show Docstring (Insert)' })
-      vim.keymap.set('i', '<C-s>', vim.lsp.buf.signature_help, { desc = 'Show Signature (Insert)' })
-      -- Double Esc to exit Terminal mode
-      vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+        -- Define Astral's 'ty' natively (completely bypassing lspconfig)
+        vim.lsp.config("ty", {
+          cmd = { "ty", "server" },
+          filetypes = { "python" },
+          -- In 0.11, 'root_markers' natively replaces the old lspconfig.util.root_pattern
+          root_markers = { "uv.lock", "pyproject.toml", ".git" }, 
+          single_file_support = true,
+        })
+
+        -- Enable the servers (This natively replaces lspconfig.SERVER.setup{})
+        vim.lsp.enable("ty")
+        vim.lsp.enable("ruff")
+
+        -- 3. LSP Keymaps
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, { desc = 'Show Docstring (Normal)' })
+        vim.keymap.set('i', '<C-k>', vim.lsp.buf.hover, { desc = 'Show Docstring (Insert)' })
+        vim.keymap.set('i', '<C-s>', vim.lsp.buf.signature_help, { desc = 'Show Signature (Insert)' })
+        vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
     end
   },
 
@@ -99,10 +150,25 @@ require("lazy").setup({
       vim.keymap.set('n', '<leader>rp', ':vsplit | term ipython<CR>', { desc = 'Open IPython REPL' })
 
       -- Map Ctrl+Enter (<C-CR>) to send code
+      -- NB: this doesn't work for some terminals, such as Windows Terminal
       vim.keymap.set('n', '<C-CR>', '<Plug>SlimeParagraphSend', { desc = 'Send Paragraph' })
       vim.keymap.set('x', '<C-CR>', '<Plug>SlimeRegionSend', { desc = 'Send Selection' })
 
     end
   }
+})
+
+-- Format Python files on save using Ruff
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.py",
+  callback = function(args)
+    vim.lsp.buf.format({
+      bufnr = args.buf,
+      -- Force Neovim to use Ruff for formatting, ignoring 'ty' if it also tries to format
+      filter = function(client)
+        return client.name == "ruff"
+      end
+    })
+  end,
 })
 
